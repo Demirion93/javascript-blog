108c202124feb73ac1e07a44434d511b780abf55
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-05-25 18:41:53 +0200

    Add js code for task 6.3

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -15,25 +16,40 @@
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-    clickedElement.classList.add('active')
     }
   
     /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts article.active');{
 
-    for(let aactiveArticle of activeArticles){
-    activeArticle.classList.remove('active');
+    for(let activeArticle of activeArticles){
+    clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
     }
   
     /* [DONE] remove class 'active' from all articles */
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    console.log('clickedElement:', clickedElement);
+    }
+  }
   
     /* get 'href' attribute from the clicked link */
+
+      const articleSelector = clickedElement.getAttribute('href');{
+      console.log(articleSelector);
+    }
   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
+    const targetArticle = document.querySelector(articleSelector);{
+    console.log(targetArticle);
+    
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+    }
   }
   
   const links = document.querySelectorAll('.titles a');
